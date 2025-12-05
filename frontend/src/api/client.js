625431d0296b79e25509debde204d6b08c2def43
Ashif EK <ashifek11@gmail.com>
2025-12-05 10:52:49 +0530

    fix: update DEBUG setting in settings.py and adjust API baseURL in client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://django-jwt-fastapi-blog-backend-1.onrender.com/api/",
-  // baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: "https://django-jwt-fastapi-blog-backend-1.onrender.com/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
